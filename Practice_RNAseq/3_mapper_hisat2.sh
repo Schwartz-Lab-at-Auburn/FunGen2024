962641d203463227e3746538d23bc4c7f875bcce
tschwart <schwartz.tonia@gmail.com>
2024-02-04 18:11:08 -0600

    Update 3_mapper_hisat2.sh

--- Practice_RNAseq/3_mapper_hisat2.sh
+++ Practice_RNAseq/3_mapper_hisat2.sh
@@ -10,8 +10,8 @@
 ##                    Output: Indexed genome 
 ## HiSat2 Mapping     Input: Cleaned read files, paired (.fasq); Indexed genome
 ##                    Output: Alignment .sam files  
-## Samtools  Convert .sam to .bam and sort         Input: Alignment files,  .sam
-##                                                  Output: Sorted, .bam files
+## Samtools  Convert .sam to .bam and sort          Input: Alignment files,  .sam
+##                                                  Output: Sorted  .bam files
 ## Stringtie  Counting reads  Input: sorted .bam file
 ##                            Output:  Directories of counts files for Ballgown (R program for DGE)
 ##              prepDE.py    Python script to create a counts matrics from the Stringtie output.  Inputs: Directory from Stringtie
@@ -54,7 +54,7 @@ set -x
 MyID=[1]          ## Example: MyID=aubtss
 
 WD=[2]            ## Example:/scratch/$MyID/PracticeRNAseq  
-CLEAND=[3]        ## Example:/scratch/$MyID/PracticeRNAseq/CleanData20   #   *** This is where the cleaned paired files are located
+CD=[3]            ## Example:/scratch/$MyID/PracticeRNAseq/CleanData20   #   *** This is where the cleaned paired files are located from the last script
 REFD=[4]          ## Example:/scratch/$MyID/PracticeRNAseq/DaphniaRefGenome    # this directory contains the indexed reference genome for the garter snake
 MAPD=[5]          ## Example:/scratch/$MyID/PracticeRNAseq/Map_HiSat2      #
 COUNTSD=[6]       ## Example:/scratch/$MyID/PracticeRNAseq/Counts_StringTie
@@ -70,50 +70,50 @@ mkdir -p $RESULTSD
 
 ##################  Prepare the Reference Index for mapping with HiSat2   #############################
 cd $REFD
-cp /home/shared/schwartz_class/$REF.fasta .
-cp /home/shared/schwartz_class/$REF.gff3 .
+cp /home/${MyID}/class_shared/${REF}.fasta .
+cp /home/${MyID}/class_shared/${REF}.gff3 .
 
 ###  Identify exons and splice sites
-gffread $REF.gff3 -T -o $REF.gtf               ## gffread converts the annotation file from .gff3 to .gft formate for HiSat2 to use.
-extract_splice_sites.py $REF.gtf > $REF.ss
-extract_exons.py $REF.gtf > $REF.exon
+gffread ${REF}.gff3 -T -o ${REF}.gtf               ## gffread converts the annotation file from .gff3 to .gft formate for HiSat2 to use.
+extract_splice_sites.py ${REF}.gtf > ${REF}.ss
+extract_exons.py ${REF}.gtf > ${REF}.exon
 
 #### Create a HISAT2 index for the reference genome. NOTE every mapping program will need to build a its own index.
-hisat2-build --ss $REF.ss --exon $REF.exon $REF.fasta DpulPA42_index
+hisat2-build --ss ${REF}.ss --exon ${REF}.exon ${REF}.fasta DpulPA42_index
 
 ########################  Map and Count the Data using HiSAT2 and StringTie  ########################
 
 # Move to the data directory
-cd $CLEAND  #### This is where our clean paired reads are located.
+cd ${CD}  #### This is where our clean paired reads are located.
 
 ## Create list of fastq files to map.    Example file format of your cleaned reads file names: SRR629651_1_paired.fastq SRR629651_2_paired.fastq
 ## grab all fastq files, cut on the underscore, use only the first of the cuts, sort, use unique put in list
 ls | grep ".fastq" |cut -d "_" -f 1| sort | uniq > list    #should list Example: SRR629651
 
 ## Move to the directory for mapping
-cd $MAPD
+cd ${MAPD}
 
 ## move the list of unique ids from the original files to map
-mv $CLEAND/list  . 
+mv ${CD}/list  . 
 
 while read i;
 do
   ## HiSat2 is the mapping program
   ##  -p indicates number of processors, --dta reports alignments for StringTie --rf is the read orientation
    hisat2 -p 6 --dta --phred33       \
-    -x "$REFD"/DpulPA42_index       \
-    -1 "$CLEAND"/"$i"_1_paired.fastq  -2 "$CLEAND"/"$i"_2_paired.fastq      \
+    -x "${REFD}"/DpulPA42_index       \
+    -1 "${CD}"/"$i"_1_paired.fastq  -2 "${CD}"/"$i"_2_paired.fastq      \
     -S "$i".sam
 
     ### view: convert the SAM file into a BAM file  -bS: BAM is the binary format corresponding to the SAM text format.
     ### sort: convert the BAM file to a sorted BAM file.
     ### Example Input: SRR629651.sam; Output: SRR629651_sorted.bam
-  samtools view -@ 6 -bS "$i".sam > "$i".bam  ### This works on ASC
+  samtools view -@ 6 -bS "$i".sam > "$i".bam  #
 
-    ###  This is sorting the bam
+    ###  This is sorting the bam, using 6 threads, and producing a .bam file that includes the word 'sorted' in the name
   samtools sort -@ 6  "$i".bam    "$i"_sorted
 
-    ### Index the BAM and get mapping statistics
+    ### Index the BAM and get mapping statistics, and put them in a text file for us to look at.
   samtools flagstat   "$i"_sorted.bam   > "$i"_Stats.txt
 
   ### Stringtie is the program that counts the reads that are mapped to each gene, exon, transcript model. 
@@ -121,18 +121,20 @@ do
   ### Original: This will make transcripts using the reference geneome as a guide for each sorted.bam
   ### eAB options: This will run stringtie once and  ONLY use the Ref annotation for counting readsto genes and exons 
   ###
-mkdir "$COUNTSD"/"$i"
-stringtie -p 6 -e -B -G  "$REFD"/"$REF".gtf -o "$COUNTSD"/"$i"/"$i".gtf -l "$i"   "$MAPD"/"$i"_sorted.bam
+mkdir "${CD}"/"$i"
+stringtie -p 6 -e -B -G  "${REFD}"/"${REF}".gtf -o "${CD}"/"$i"/"$i".gtf -l "$i"   "${MAPD}"/"$i"_sorted.bam
 
 done<list
 
 #####################  Copy Results to home Directory.  These will be the files you want to bring back to your computer.
 ### these are your stats files from Samtools
-cp *.txt $RESULTSD
+cp *.txt ${RESULTSD}
 
-### The prepDE.py is a python script that converts the files in your ballgown folder to a count matrix
-cd $COUNTSD
-python /home/$MyID/class_shared/prepDE.py -i $COUNTSD
+### The prepDE.py is a python script that converts the files in your ballgown folder to a count matrix. 
+ ## Move to the counts directory
+cd ${CD}
+ ## run the python script prepDE.phy to prepare you data for downstream analysis.
+python /home/${MyID}/class_shared/prepDE.py -i ${CD}
 
 ### copy the final results files (the count matricies that are .cvs to your home directory)
-cp *.csv $RESULTSD
+cp *.csv ${RESULTSD}
